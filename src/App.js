dc1fb36a10257349a0403a4a3881bf4fc99b598f
rbillar <rbillar@gmail.com>
2023-02-11 03:06:26 +0000

    update and bug fix

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
+import {Header} from './Header'
 import './App.css';
 
-function App() {
+const App = () => {
 
-  const name = 'Rudy Billar';
-
-  const handleClick = () => {
-    alert("This is a JavaScript alert in React");
-  };
   return (
     <div className="App">
-      <header className="App-header">
-        {name}
-
-        <button onClick={handleClick}>Show Alert </button>
-        
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-         <a href="{alert('all good!');}">click me</a>
-        <p>
-          
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <h3>Rudy Billar</h3>
+     
     </div>
   );
+
+ 
 }
 
 export default App;
